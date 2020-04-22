5488b063c6ea69ecfd9b55dc455a531cbb66768c
httpsantos <carlos.hrq.rodrigues@gmail.com>
2020-04-22 14:45:23 -0300

    tratando importação de categorias

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,17 +1,31 @@
+import { getCustomRepository, getRepository, In } from 'typeorm';
 import csvParse from 'csv-parse';
 import fs from 'fs';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface CSVTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
   async execute(filePath: string): Promise<Transaction[]> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoriesRepository = getRepository(Category);
+
     const contactsReadStream = fs.createReadStream(filePath);
 
     const parsers = csvParse({
       from_line: 2,
     });
 
-    const transactions = [];
-    const categories = [];
+    const transactions: CSVTransaction[] = [];
+    const categories: string[] = [];
 
     const parserCSV = contactsReadStream.pipe(parsers);
 
@@ -28,10 +42,27 @@ class ImportTransactionsService {
 
     await new Promise(resolve => parserCSV.on('end', resolve));
 
-    return {
-      categories,
-      transactions,
-    };
+    const existentCategories = await categoriesRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    const existentCategoriesTitle = existentCategories.map(
+      (category: Category) => category.title,
+    );
+
+    const addCategoryTitles = categories
+      .filter(category => !existentCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = categoriesRepository.create(
+      addCategoryTitles.map(title => ({
+        title,
+      })),
+    );
+
+    await categoriesRepository.save(newCategories);
   }
 }
 
