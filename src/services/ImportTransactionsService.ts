3dfeb9834c8875a40826dadc842fcc200c231138
httpsantos <carlos.hrq.rodrigues@gmail.com>
2020-04-22 10:15:08 -0300

    fazendo o upload dos arquivos

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,37 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
 import Transaction from '../models/Transaction';
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const contactsReadStream = fs.createReadStream(filePath);
+
+    const parsers = csvParse({
+      from_line: 2,
+    });
+
+    const transactions = [];
+    const categories = [];
+
+    const parserCSV = contactsReadStream.pipe(parsers);
+
+    parserCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parserCSV.on('end', resolve));
+
+    return {
+      categories,
+      transactions,
+    };
   }
 }
 
