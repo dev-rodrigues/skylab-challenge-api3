5524c2ad6164f74eb90a17d1fc9c5f910f822e15
httpsantos <carlos.hrq.rodrigues@gmail.com>
2020-04-21 23:10:39 -0300

    apagando uma transacao

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,25 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getConnection } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await getConnection()
+      .createQueryBuilder()
+      .delete()
+      .from(Transaction)
+      .where('id = :id', { id })
+      .execute();
   }
 }
 
